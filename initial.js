25e2982eec01f5b827bafcde581e658c43e5578b
avwo <avwu@qq.com>
2020-06-02 15:47:02 +0800

    refactor: use inline values instead of temp files

--- initial.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const rmdir = require('licia/rmdir');
-const mkdir = require('licia/mkdir');
-
-const scriptsPath = path.resolve(__dirname, './scripts');
-rmdir(scriptsPath, () => mkdir(scriptsPath));
-
-module.exports = (options) => options;
